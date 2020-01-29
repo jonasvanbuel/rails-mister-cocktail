07e5de1e29730def379d05218fb6c38199f88e25
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2020-01-29 18:35:19 +0000

    Backend almost complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,13 +9,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 lemon_juice = Ingredient.create(name: "lemon juice")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+ice = Ingredient.create(name: "ice")
+mint_leaves = Ingredient.create(name: "mint leaves")
 whiskey = Ingredient.create(name: "whiskey")
 bourbon = Ingredient.create(name: "bourbon")
 
 whiskey_sour = Cocktail.create(name: "Whiskey sour")
-Cocktail.create(name: "Mojito")
+ws_img = URI.open('https://res.cloudinary.com/deo4sjfc8/image/upload/v1580240903/djjxf9ty0haqdid6kkia.jpg')
+whiskey_sour.photo.attach(io: ws_img, filename: 'ws_img.jpg', content_type: 'image/png')
+
+mojito = Cocktail.create(name: "Mojito")
+mj_img = URI.open('https://res.cloudinary.com/deo4sjfc8/image/upload/v1580242744/psth9zhfu6xdt23u597u.jpg')
+mojito.photo.attach(io: mj_img, filename: 'mj_img.jpg', content_type: 'image/png')
 
 Dose.create(description: "50ml", ingredient: bourbon, cocktail: whiskey_sour)
 Dose.create(description: "35ml", ingredient: lemon_juice, cocktail: whiskey_sour)
